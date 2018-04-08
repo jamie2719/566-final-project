2ce4d7179f1e174789a45a0cafdaca0d2ae240ff
Kathryn Miller <kamill@seas.upenn.edu>
2018-04-08 09:18:12 -0400

    obj added whoops

--- src/shaders/examplePost2-frag.glsl
+++ src/shaders/examplePost2-frag.glsl
@@ -9,9 +9,5 @@ uniform float u_Time;
 
 // Render R, G, and B channels individually
 void main() {
-	out_Col = vec4(texture(u_frame, fs_UV + vec2(0.33, 0.0)).r,
-								 texture(u_frame, fs_UV + vec2(0.0, -0.33)).g,
-								 texture(u_frame, fs_UV + vec2(-0.33, 0.0)).b,
-								 1.0);
- out_Col.rgb += texture(u_frame, fs_UV).xyz;
+	out_Col = texture(u_frame, fs_UV);
 }
--- src/shaders/examplePost3-frag.glsl
+++ src/shaders/examplePost3-frag.glsl
@@ -10,12 +10,5 @@ uniform float u_Time;
 // Interpolate between regular color and channel-swizzled color
 // on right half of screen. Also scale color to range [0, 5].
 void main() {
-	vec3 color = texture(u_frame, fs_UV).xyz;
-	color += 10.0 * max(color - 0.5, vec3(0.0)); // color is not clamped to 1.0 in 32 bit color
-
-	vec3 color2 = color.brg;
-	float t = 0.5 + 0.5 * cos(1.5 * 3.14 * (u_Time + 0.25));
-	t *= step(0.5, fs_UV.x);
-	color = mix(color, color2, smoothstep(0.0, 1.0, t));
-	out_Col = vec4(color, 1.0);
+	out_Col = texture(u_frame, fs_UV);
 }
